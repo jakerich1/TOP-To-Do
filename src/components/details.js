0e1337b8c49aa4634c34a169ff3b55d7f5f06007
Jacob R <jakeriches1@gmail.com>
2021-06-25 16:38:23 +0000

    Placeholder elements finished

--- src/components/details.js
+++ src/components/details.js
@@ -4,7 +4,62 @@ export default function details() {
     const details = document.createElement('div')
     details.classList.add('detail-area')
 
-    details.innerHTML = "test"
+    //Details heading area
+    const head = document.createElement('div')
+    head.classList.add('head')
+    head.innerText = "Details"
+    details.appendChild(head)
+
+    //Task title area
+    const title = document.createElement('div')
+    title.classList.add('title', 'detail-cont')
+    const titleHead = document.createElement('div')
+    titleHead.classList.add('heading')
+    titleHead.innerText = "Title"
+    title.appendChild(titleHead)
+    const titleContent = document.createElement('input')
+    titleContent.value = "Javascript Chapter"
+    title.appendChild(titleContent)
+    details.appendChild(title)
+
+    //Task Description
+    const description = document.createElement('div')
+    description.classList.add('description', 'detail-cont')
+    const descTitle = document.createElement('div')
+    descTitle.classList.add('heading')
+    descTitle.innerText = "Description"
+    description.appendChild(descTitle)
+    const descContent = document.createElement('textarea')
+    descContent.innerText = "Enter task description here"
+    description.appendChild(descContent)
+    details.appendChild(description)
+
+    //Priority area
+    const priority = document.createElement('div')
+    priority.classList.add('priority', 'detail-cont')
+    const priorityHead = document.createElement('div')
+    priorityHead.classList.add('heading')
+    priorityHead.innerText = "Priority"
+    priority.appendChild(priorityHead)
+    const priorityContent = document.createElement('input')
+    priorityContent.value = "High"
+    priority.appendChild(priorityContent)
+    details.appendChild(priority)
+
+    //Due date area
+    const dueDate = document.createElement('div')
+    dueDate.classList.add('due-date', 'detail-cont')
+    const dueHead = document.createElement('div')
+    dueHead.classList.add('heading')
+    dueHead.innerText = 'Due-Date'
+    dueDate.appendChild(dueHead)
+    const dueContent = document.createElement('input')
+    dueContent.type = 'date'
+    dueContent.value = "2021-06-21"
+    dueDate.appendChild(dueContent)
+
+
+    details.appendChild(dueDate)    
 
     return details
 
